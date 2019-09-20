7104ae1b6d2410efc22e810334a6d48b5d3d369d
Oliver.Lin <Oliver.Lin@avepoint.com>
2019-09-20 16:29:11 +0800

    [LeetCode-767]给定一个字符串S，检查是否能重新排布其中的字母，使得两相邻的字符不同。

---
+++ PracticeCodeSolution/CharRestructure/CharTool.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharRestructure
+{
+    public class CharTool
+    {
+        public string ReorganizeString(string oldStr)
+        {
+            string reorganizeStr = string.Empty;
+            if (oldStr == null || oldStr.Length == 0)
+            {
+                return "";
+            }
+            int[] charCount = new int[26];
+            foreach (char item in oldStr.ToCharArray())
+            {
+                charCount[item - 'a']++;
+            }
+            int maxCharCount = 0;
+            char maxCountLetter='0';
+            for (int i = 0; i < charCount.Length; i++)
+            {
+                if (maxCharCount < charCount[i])
+                {
+                    maxCharCount = charCount[i];
+                    maxCountLetter = (char)('a' + i);
+                }
+            }
+            if (maxCharCount > (oldStr.Length + 1) / 2)
+            {
+                return "";
+            }
+            char[] res = new char[oldStr.Length];
+            int index = insert(charCount, res, 0, maxCountLetter);
+            for (int i = 0; i < 26; i++)
+            {
+                index = insert(charCount, res, index, (char)('a' + i));
+            }
+            reorganizeStr = new string(res);
+            return reorganizeStr;
+        }
+
+        private int insert(int[] oldCount,char[] res,int index, char letter)
+        {
+            int i = letter - 'a';
+            while (oldCount[i] > 0)
+            {
+                if (index >= res.Length)
+                {
+                    index = 1;
+                }
+                res[index] = letter;
+                oldCount[i]--;
+                index += 2;
+            }
+            return index;
+        }
+    }
+}
---
+++ PracticeCodeSolution/CharRestructure/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CharRestructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start!");
+            CharTool charTool = new CharTool();
+            List<string> testStrList = new List<string> { "aab","aaabbcc","aaabb","aaaaabbbcc","aaaaabbc","aaab"};
+            Console.WriteLine("Before      After");
+            foreach (var testStr in testStrList)
+            {
+                string afterStr= charTool.ReorganizeString(testStr);
+                if (!string.IsNullOrEmpty(afterStr))
+                {
+                   Console.WriteLine($"{testStr}        {afterStr}");
+                }
+                else
+                {
+                    Console.WriteLine($"{testStr}        {afterStr}");
+                }
+            }            
+            Console.WriteLine("Finished!");
+
+            Console.ReadKey();
+        }
+    }
+}
