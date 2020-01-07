30d1ed611118daee667a63d60698335b7e2e2d8a
linjiru@outlook.com <QWEr19960217>
2020-01-07 17:53:19 +0800

    [1040_MoveStones]move stones

---
+++ PracticeCodeSolution/1040_MoveStones/MoveStonesTool.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1040_MoveStones
+{
+    class MoveStonesTool
+    {
+        public int[] numMovesStone(int[] stones)
+        {            
+            int maxMove = 0;
+            Array.Sort(stones);
+            int n = stones.Length;
+            int noValueLength = stones[n - 1] - stones[0] + 1 - n;
+            maxMove = noValueLength - Math.Min(stones[n-1]-stones[n-2] + 1 - 2, stones[1]-stones[0]+1-2);
+            int minMove = n;
+            for (int i = 0, j = 0; j < n; ++j)
+            {
+                //当前窗口的大小大于n
+                while (stones[j] - stones[i] + 1 > n) i++;
+
+                //当前窗口的石子个数
+                int already_stone = (j - i + 1);
+
+                //前n-1个石子的顺序连续，最后一个石子不连续，需要移动2步。
+                //比如3 4 5 6 10，我们不能直接将10变为2，而是将3变为8，然后10变为7来移动
+                if (already_stone == n - 1 && stones[j] - stones[i] + 1 == n - 1)
+                {
+                    minMove = Math.Min(minMove, 2);
+                }
+                else
+                {
+                    minMove = Math.Min(minMove, n - already_stone);
+                }
+            }
+                
+            return  new int[2]{ minMove, maxMove };
+        } 
+
+        public void MaxMoveslIne(int [] stones)
+        {
+            int maxMove = 0;
+            //Array.Sort(stones);
+            int n = stones.Length;
+            int noValueLength = stones[n - 1] - stones[0] + 1 - n;
+            int lossNoValueLengthByLeftMove = stones[1] - stones[0] + 1 - 2;
+            int lossNoValueLengthByRightMove = stones[n - 1] - stones[n - 2] + 1 - 2;
+            if (noValueLength > 0)
+            {
+                if (lossNoValueLengthByLeftMove < lossNoValueLengthByRightMove)
+                {
+                    int temp = 0;
+                    temp = stones[0];
+                    stones[0] = stones[1];
+                    stones[1] = temp;                    
+                }
+                else
+                {
+                    int temp = 0;
+                    temp = stones[n-1];
+                    stones[n-1] = stones[n-2];
+                    stones[n-2] = temp;
+                }
+                Console.WriteLine(stones.ToString());
+                MaxMoveslIne(stones);
+            }            
+        }
+    }
+}
---
+++ PracticeCodeSolution/1040_MoveStones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1040_MoveStones
+{
+    class Program
+    {
+    //在一个长度无限的数轴上，第 i 颗石子的位置为 stones[i]。如果一颗石子的位置最小/最大，那么该石子被称作端点石子。
+
+    //每个回合，你可以将一颗端点石子拿起并移动到一个未占用的位置，使得该石子不再是一颗端点石子。
+
+    //值得注意的是，如果石子像 stones = [1, 2, 5]这样，你将无法移动位于位置 5 的端点石子，因为无论将它移动到任何位置（例如 0 或 3），该石子都仍然会是端点石子。
+
+    //当你无法进行任何移动时，即，这些石子的位置连续时，游戏结束。
+
+    //要使游戏结束，你可以执行的最小和最大移动次数分别是多少？ 以长度为 2 的数组形式返回答案：answer = [minimum_moves, maximum_moves] 。
+
+
+        static void Main(string[] args)
+        {
+            int[] stones = new int[] {6,5,4,3,10};
+            MoveStonesTool tool = new MoveStonesTool();
+            var result=tool.numMovesStone(stones);
+            Console.WriteLine("MinMove:{0},MaxMove:{1}",result[0],result[1]);
+            tool.MaxMoveslIne(stones);
+            Console.ReadKey();
+        }
+    }
+}
